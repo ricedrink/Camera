41643d7d7c695b362ac18bf4bbf9cd3100d598bd
Alice Zhang <alicezhang32092@gmail.com>
2019-06-24 13:37:48 -0700

    first

--- camera/ViewController.swift
+++ camera/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var newScreen = UIImagePickerController()
+    
+    @IBAction func takePhotoTapped(_ sender: UIButton) {
+        newScreen.sourceType = .camera
+            present(newScreen, animated: true, completion: nil)
     }
-
+    
+    @IBOutlet weak var photoTaken: UIImageView!
+    
+    //last step
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage { photoTaken.image = selectedImage
+            
+        }
+        newScreen.dismiss(animated: true, completion: nil)
+    }
 
 }
 
